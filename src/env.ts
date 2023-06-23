99bb5d8b64c75dba3118190e2999584c9109745f
Hellodshboom01 <134821918+Hellodshboom01@users.noreply.github.com>
2023-06-23 20:38:10 +0800

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -14,16 +14,16 @@ export const defaultEnv = {
     title: "",
     saveSession: true,
     // 0-2
-    APITemperature: 0.6,
+    APITemperature: 0.7,
     continuousDialogue: true,
     model: "gpt-3.5" as SimpleModel
   },
   CLIENT_DEFAULT_MESSAGE: `Powered by OpenAI Vercel
-- 如果本项目对你有所帮助，可以给小猫 [买点零食](https://cdn.jsdelivr.net/gh/ourongxing/chatgpt-vercel/assets/reward.gif)，但不接受任何付费功能请求。
-- 本网站仅作为项目演示，不提供服务，请填入自己的 Key，长期使用请 [自行部署](https://github.com/ourongxing/chatgpt-vercel#%E9%83%A8%E7%BD%B2%E4%B8%80%E4%B8%AA%E4%BD%A0%E8%87%AA%E5%B7%B1%E7%9A%84-chatgpt-%E7%BD%91%E7%AB%99%E5%85%8D%E8%B4%B9)，简单成本低。
-- 点击每条消息前的头像，可以锁定对话，作为角色设定。[查看更多使用技巧](https://github.com/ourongxing/chatgpt-vercel#使用技巧)。
-- 现在支持多个对话，打开对话设置，点击新建对话。在输入框里输入 [[/]][[/]] 或者 [[空格]][[空格]] 可以切换对话，搜索历史消息。
-- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。点击顶部名称滚动到顶部，点击输入框滚动到底部。
+- dshboom的AI助手 [请我喝杯咖啡](https://image.cdn.dshboom.top/post/202306232035402.png)。
+- 默认提供最新的gpt-3.5-turbo-16k模型，给大家免费使用，[点击进群获取密码](https://image.cdn.dshboom.top/post/202306232037165.png)
+- 为保障正常请求，本网站关闭了自填key的通道。如果你有相关需要，可以邮箱联系dshboom@outlook.com
+- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 搜索 Prompt 预设。[[↑]] 可编辑最近一次提问。
+- [点此处获取更多使用技巧](https://github.com/dshboom/free-gpt-dshboom#%E4%BD%BF%E7%94%A8%E6%8A%80%E5%B7%A7)
 `,
   CLIENT_MAX_INPUT_TOKENS: {
     "gpt-3.5": 16 * 1000,
